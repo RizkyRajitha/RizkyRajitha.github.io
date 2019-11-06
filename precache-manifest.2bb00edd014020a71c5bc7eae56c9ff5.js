ff2f4b8574532c422eacc845d53d699788cc4ff5
RizkyRajitha <rajithagunathilake@gmail.com>
2019-11-06 20:47:58 +0530

    added comments section

---
+++ precache-manifest.2bb00edd014020a71c5bc7eae56c9ff5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "15c126211ad29c6a5b360d5ea50a3ea3",
+    "url": "/static/media/998152.15c12621.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "463577035c30bd45a0c8",
+    "url": "/static/js/main.e51751df.chunk.js"
+  },
+  {
+    "revision": "c68d814ae364df13aa1c",
+    "url": "/static/js/2.04653889.chunk.js"
+  },
+  {
+    "revision": "463577035c30bd45a0c8",
+    "url": "/static/css/main.cbb5f9cb.chunk.css"
+  },
+  {
+    "revision": "04ea1cafc5d30a1612d9413a78a22c72",
+    "url": "/index.html"
+  }
+];
