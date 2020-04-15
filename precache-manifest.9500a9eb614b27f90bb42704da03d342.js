3786346610b477feca0e402031cfaae01c276a8d
RizkyRajitha <rajithagunathilake@gmail.com>
2020-04-15 17:28:52 +0530

    added portfolio

---
+++ precache-manifest.9500a9eb614b27f90bb42704da03d342.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b9170abb0f4bf95d679b",
+    "url": "/static/js/main.8c56b5ea.chunk.js"
+  },
+  {
+    "revision": "fd30f20ff8cf68030e0f",
+    "url": "/static/js/2.cd20fad1.chunk.js"
+  },
+  {
+    "revision": "b9170abb0f4bf95d679b",
+    "url": "/static/css/main.de4cf8bb.chunk.css"
+  },
+  {
+    "revision": "8e8a31bf68a490f1eeca8c51a8edbae1",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.536ef1cf24b0a201f93925566942b449.js"
+  "/precache-manifest.9500a9eb614b27f90bb42704da03d342.js"
 );
 
 workbox.clientsClaim();
